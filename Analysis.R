3e4ecc44f26a1b7c1dfe26d8ac21a93ffa569900
ndub6 <nweitershausen@gmail.com>
2026-09-02 11:18:28 -0700

    update

--- Analysis.R
+++ Analysis.R
@@ -1,5 +1,9 @@
 # first r script in new Git repo
+# Using R
+# Option 1: tidytuesdayR R package 
+install.packages("tidytuesdayR")
 
-plot(1:10)
-summary(1:100)
+tuesdata <- tidytuesdayR::tt_load('2026-09-01')
 
+
+world_castles <- tuesdata$world_castles
